957424e04ca4a862e2dbb5c8167cdb45b16c1378
IbnuParvez <parvezfatma98@gmail.com>
2025-11-30 19:44:44 +0300

    solved controller issue

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const container = document.querySelector('.container');
+console.log('Container found:', container);
 const gridSize = 16;
 
 function createGrid(n){
+  console.log('Creating grid with size', n);
   for(let i = 0; i < n*n; i++) {
-      let gridItem=document.createElement('div');
-      gridItem.classList.add('div');
-      container.appendChild(gridItem);
+      const div=document.createElement('div');
+      div.classList.add('grid-item');
+      container.appendChild(div);
   }
+  console.log('Grid created, total divs:', container.children.length);
 }
 
 createGrid(gridSize);
+
+const gridItems = document.querySelectorAll('.grid-item');
+gridItems.forEach(item => {
+  item.addEventListener('mouseenter', () => { item.style.backgroundColor = 'black'; });
+});
